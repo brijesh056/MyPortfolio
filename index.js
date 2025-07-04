f419838fd75c4b3c3f5604c8627bb2f8f14133f2
Brijesh <konkanibrijesh999@gmail.com>
2025-07-04 23:34:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+ src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"
+var typed = new Typed('#element', {
+      strings: ['Web Devloper', 'Graphic Designer','Video Editor'],
+      typeSpeed: 50,
+    });
